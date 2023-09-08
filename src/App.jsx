2737e475803bea173b14dd8a69728b17fb7e28fd
sid2261 <siddhartha2251@gmail.com>
2023-09-08 10:11:15 +0530

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import{ css } from "@emotion/react";
-import { Outlet } from 'react-router-dom';
-import ContactList from './contactList';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { css } from "@emotion/react";
+import { Outlet } from "react-router-dom";
+import ContactList from "./ContactList";
+import { useRef } from "react";
 
+function Header({ onSearch }) {
+  const searchElementRef = useRef(null);
+
+  function onSubmit(e) {
+    e.preventDefault();
+    onSearch(searchElementRef.current.value);
+  }
+
+  return (
+    <header
+      css={css`
+        border-bottom: 1px solid;
+        padding: 16px;
+      `}
+    >
+      <form
+        css={css`
+          display: flex;
+          justify-content: space-between;
+        `}
+        onSubmit={onSubmit}
+      >
+        <input
+          ref={searchElementRef}
+          css={css`
+            padding: 8px;
+          `}
+          type="text"
+        />
+        <button>Search</button>
+      </form>
+    </header>
+  );
+}
 
 function Sidenav() {
+  const [searchText, setSearchText] = useState("");
+  function onSearch(text) {
+    setSearchText(text);
+  }
+
   return (
     <aside
       css={css`
@@ -15,60 +54,50 @@ function Sidenav() {
         display: grid;
         grid-template-rows: auto 1fr auto;
       `}
-      >
-      <header css={css`
-          border-bottom: 1px solid;
-          padding: 16px;
-          `}
-        >
-        <form 
-            css={css`
-            display: flex;
-            justify-content: space-between;
-            `}
-          >
-          <input type='text'/>
-          <button>Search</button>
-        </form>
-      </header>
+    >
+      <Header onSearch={onSearch} />
       <section
         css={css`
-            padding:16px;
-            `}
-            >
-            <ContactList/>
-            </section>
+          padding: 16px;
+          height: 500px;
+          overflow: auto;
+        `}
+      >
+        <ContactList searchText={searchText} />
+      </section>
       <footer
         css={css`
           border-top: 1px solid;
           padding: 16px;
-          `}
-        >
-        footer</footer>
+        `}
+      >
+        footer
+      </footer>
     </aside>
-  )
+  );
 }
-function Contents(){
+
+function Contents() {
   return (
     <section>
-    <Outlet/>
+      <Outlet />
     </section>
-  )
+  );
 }
 
-
-
-export function Layout(){
+export function Layout() {
   return (
     <>
-      <main css={css`
-        display:grid ;
-        grid-template-columns: minmax(250px, 25%) 1fr;
-        height: 100%;
-      `}>
-        <Sidenav/>
-        <Contents/>
+      <main
+        css={css`
+          display: grid;
+          grid-template-columns: minmax(250px, 25%) 1fr;
+          height: 100%;
+        `}
+      >
+        <Sidenav />
+        <Contents />
       </main>
     </>
-  )
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Layout } from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Layout } from "./App";
+import "./index.css";
 import { createBrowserRouter } from "react-router-dom";
 import { createRoutesFromElements } from "react-router-dom";
 import { Route } from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
-import { loadContacts } from './loaders.js';
-
+import { loadContact, loadContacts } from "./loaders";
+import Contact from "./Contact";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-      <Route path="/" element={<Layout/>} loader={loadContacts}>
-        <Route path="/contact/:contactId" element={<div>contacts</div>}/>
-      </Route>
+    <Route path="/" element={<Layout />} loader={loadContacts}>
+      <Route path="/contacts/:contactId" loader={({ params }) => loadContact(params.contactId)} element={<Contact />} />
+      <Route
+        path="/edit/:contactId"
+        loader={({ params }) => loadContact(params.contactId)}
+        element={<h1>edit contact</h1>}
+      />
+    </Route>
   )
 );
-   
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
- 
